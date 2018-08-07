81726f835371ee7f4d0d68ad776f28b9be2ba036
gwenananaelle <gwenaelle.duchesne@gmail.com>
2018-08-07 10:27:05 +0200

    pong

--- src/server.js
+++ src/server.js
@@ -1,1 +1,14 @@
-console.log('how are you ?');
+import express from 'express'
+const app = express()
+
+app.get('/ping', (req, res) => {
+//   res.send('pong')
+    res 
+        .status(200)
+        .setHeader('content-type', 'text/html')
+    res.send('pong')
+})
+
+app.listen(5000, function () {
+  console.log('Example app listening on port 5000!')
+})
