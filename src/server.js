d60360ab18d5bc23052846351b9b861c1319b3c7
gwenananaelle <gwenaelle.duchesne@gmail.com>
2018-08-07 11:05:44 +0200

    add json

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
 import express from 'express'
+// import fs from 'fs'
 const app = express()
+const list = require('./data/list.json')
+// const list = fs.readFileSync('src/data/list.json','utf8')
 
 app.get('/ping', (req, res) => {
 //   res.send('pong')
-    res 
-        .status(200)
-        .setHeader('content-type', 'text/html')
-    res.send('pong')
+    // res 
+    //     .status(200)
+    //     .setHeader('content-type', 'text/html')
+    // res.send(JSON.parse(list))
+    res.send(list)
 })
 
 app.listen(5000, function () {
-  console.log('Example app listening on port 5000!')
+  console.log('done')
 })
