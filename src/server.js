d699c4e5093c2ed70b425dc2a679d2d8d149a2d1
gwenananaelle <gwenaelle.duchesne@gmail.com>
2018-08-08 13:43:57 +0200

    different fetch for general and details

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 const app = express()
-const list = require('./data/list.json')
+let movies = require('./data/list.json')
+let list = null
 
 app.use(
   (req, res, next) => {
@@ -9,13 +10,35 @@ app.use(
   },
   (req, res, next) => {
     setTimeout(next, 1000);
-})
+  }
+)
 
 app.get('/movies', 
+(req, res, next) => {
+  list = movies.map(element => {return {id: element.id, title: element.title, poster: element.poster}})
+  next()
+},
 (req, res) => {
     res.send(list)
   }
 )
+app.get('/moviesDetails', 
+(req, res) => {
+    res.send(movies)
+  }
+)
+
+app.get("/movies:id", 
+(req, res) => {
+    const selectedMovie = movies.find(element => element.id == req.params.id )
+    res.send(selectedMovie)
+  }
+)
+
+app.post('/clicked', (req, res) => {
+  const click = {clickTime: new Date()};
+  console.log(click);
+});
 
 app.listen(5000, () => {
   console.log('hello');
