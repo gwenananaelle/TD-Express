6a5b32a35458e05fc21b80dbaf9d914e8b69dc39
gwenananaelle <gwenaelle.duchesne@gmail.com>
2018-08-28 17:02:36 +0200

    add edit and delete cliend side

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ app.use(
   (req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:8081')
     res.header('Access-Control-Allow-Headers', 'Content-Type')
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     next()
   },
   (req, res, next) => setTimeout(next, config.get('timeout')))
@@ -41,18 +42,43 @@ app.get("/movies:id",
     res.send(selectedMovie)
   }
 )
-
+app.delete("/movies:id",
+  (req, res) => {
+    const selectedMovie = movies.find(element => element.id == req.params.id)    
+    movies.splice(movies.indexOf(selectedMovie),1)
+    fs.writeFile(config.get('json'), JSON.stringify(movies, null, 2), (err) => {
+      if (err) throw err
+      console.log('The movie has been deleted!')
+    })
+    return res.status(204).send('film supprimé')
+  }
+)
+// Add or update a movie
 app.post('/Admin',
   (req, res) => {
     let newMovie = req.body
+    //check form for errors
     const errors = []
     const validateField = (field, msg) => (!field || field.trim().length === 0) && errors.push(msg)
     validateField(newMovie.title, 'titre obligatoire')
     validateField(newMovie.poster, 'image obligatoire')
     validateField(newMovie.summary, 'summary obligatoire')
     if (errors.length > 0) return res.status(400).send(errors)
-    newMovie = { "id": movies.length, "title": newMovie.title, "poster": newMovie.poster, "summary": newMovie.summary }
-    movies.push(newMovie)
+    //define newMovie
+    //if newMovie.id then update the movie
+    if (newMovie.id) {
+      const updatedMovie = movies.find(element => element.id == newMovie.id)
+      if (!updatedMovie) {
+        return res.status(404).send(errors)
+      } else {
+        updatedMovie.title = newMovie.title
+        updatedMovie.poster = newMovie.poster
+        updatedMovie.summary = newMovie.summary
+      }
+    } else {
+      newMovie = { "id": Date.now(), "title": newMovie.title, "poster": newMovie.poster, "summary": newMovie.summary }
+      movies.push(newMovie)
+    }
     let data = JSON.stringify(movies, null, 2);
     fs.writeFile(config.get('json'), data, (err) => {
       if (err) throw err
--- test/app.test.js
+++ test/app.test.js
@@ -42,11 +42,35 @@ describe('get /movies:id', () => {
         .end(done)
     })
 })
+describe('DELETE /movies:id', () => {
+    it('should delete the movie', done => {
+        supertest(app)
+        .delete('/movies1')
+        .expect(204)
+        .end(() => {
+            supertest(app)
+            .get('/movies1')
+            .expect(404)
+            .end(done)
+        })
+    })
+    it('should delete the movie, even if it doesn\'t exist', done => {
+        supertest(app)
+        .delete('/movies72')
+        .expect(204)
+        .end(() => {
+            supertest(app)
+            .get('/movies72')
+            .expect(404)
+            .end(done)
+        })
+    })
+})
 describe('post /admin', () => {
-    it('should be awesome', done => {
+    it('should add a movie', done => {
         supertest(app)
         .post('/admin')
-        .send({title: 'monFilm', poster: 'image1', summary:'bla bla bla'})
+        .send({title: 'movie added', poster: 'image1', summary:'bla bla bla'})
         .expect(200)
         .end(done)
     })
@@ -57,10 +81,35 @@ describe('post /admin', () => {
         .end(done)
     })
 })
+describe('post /admin', () => {
+    it('should update a movie', done => {
+        supertest(app)
+        .post('/admin')
+        .send({id:3, title: 'movie updated', poster: 'image1', summary:'bla bla bla'})
+        .expect(200)
+        .expect(({body}) => {
+            should.exist(body)
+            body.should.be.a.Object
+            body.should.have.only.keys('id', 'title', 'poster', 'summary')
+            should.equal(body.id, 3)
+            should.equal(body.title, 'movie updated')
+            should.equal(body.poster, 'image1')
+            should.equal(body.summary, 'bla bla bla')
+        })
+        .end(done)
+    })
+    it('should send a 404 error, movie updated doesn\'t exist', done => {
+        supertest(app)
+        .post('/admin')
+        .send({id:27, title: 'film inexistant', poster: 'image1', summary:'bla bla bla'})
+        .expect(404)
+        .end(done)
+    })
+})
 beforeEach(function() {
     // fs.copyFile( movieList, config.get('json'), 'src/data/list.json')
-    fs.writeFile(config.get('json'), JSON.stringify(movieList), (err) => {
+    fs.writeFile(config.get('json'), JSON.stringify(movieList, null, 2), (err) => {
         if (err) throw err
-        console.log('The file was updated!')
+        console.log('reset the file')
       })
   })
